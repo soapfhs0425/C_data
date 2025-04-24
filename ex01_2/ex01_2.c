760b9f6f9d84d04232eeca167c205a5ef4819dbb
soapfhs0425 <soapfhs0425@gmail.com>
2025-04-24 11:39:07 +0900

    수정_주석 작성

--- ex01_2/ex01_2.c
+++ ex01_2/ex01_2.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 int fun1(); // 함수 원형(함수 선언)
 int fun2();
+//함수가 main 아래위치, 해당 함수가 존재하는 것을 미리 선언
+//끝에 ;붙임
 
-int main(void) {
+int main(void) {	//void는 형 없음, 생략가능
 	printf("메인 시작\n");//1
-	fun1();//2  
+	fun1();//2  //함수호출, 인수를 보내지 않음
 	printf("메인 끝\n");//10   
-	return 0;//11
+	return 0;//11	//완전 종료
 }
-int fun1() {
+int fun1() { //매개변수도 없음, return으로 반환할 값의 형을 기입니다( return 0 -> int )
 	printf("fun1함수 시작\n");//3
-	fun2();//4
-	printf("fun1함수 끝\n");   //8
-	return 0;//9
+	fun2();//4 //함수호출
+	printf("fun1함수 끝\n");   //8	
+	return 0;//9	//fun1()를 호출한 다음 수행 줄로 이동
 }
-int fun2() {
+int fun2() {//매개변수도 없음, 
 	printf("fun2함수 시작\n");//5
 	printf("fun2함수 끝\n");//6
-	return 0;//7
+	return 0;//7	//fun2()를 호출한 다음 수행 줄로 이동
 }
+
+
+//인수를 보내고 매개변수로 받음
