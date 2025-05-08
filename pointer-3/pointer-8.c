db3d7464980dc23889a582bd9b7d34c668b3da16
soapfhs0425 <soapfhs0425@gmail.com>
2025-05-08 15:33:05 +0900

    ++

--- pointer-3/pointer-8.c
+++ pointer-3/pointer-8.c
@@ -4,13 +4,14 @@ int main()
 {
 	int i = 10;
 	int* pi = &i;
-
+	// i => 10, pi는 i 주소
 	printf("i = %d, pi = %p\n", i, pi);
 
-	(*pi)++; //주소는 그대로, 값 증가
-	printf("i = %d, pi = %p\n", i, pi);
+	(*pi)++; //pi값을 가지고 오고
+				//다음 문장으로 갈 때 값을 1증가
+	printf("i = %d, pi = %p\n", i, pi);		//11
 
-	*pi++; //주소와 값 모두 증가
+	*pi++; //pi값을 가져오고 pi주소를 증가, (*pi/++)
 	printf("i = %d, pi = %p\n", i, pi);
 	return 0;
 }
